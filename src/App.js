11eb0d3e76fa760d29a602d7e8c41faf5e957ce3
Lite Object/s <Lite.Object@gmail.com>
2022-08-23 19:53:36 +0000

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">     
+     <h1>Let's call an API endpoint</h1>
+     {/* https://datausa.io/api/data?drilldowns=Nation&measures=Population */}
+
+     <hr/>
+     Source: https://reactjs.org/docs/faq-ajax.html<br />
+     Source: https://datausa.io/about/datasets/     
+    </div>
+  );
+}
+
+export default App;
